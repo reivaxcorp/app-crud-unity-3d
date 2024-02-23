433ab4ebb57693885143bce533fa5c226d4a198c
Javier <reivaxcorp@gmail.com>
2024-02-22 21:05:12 -0300

    add icon app on UI

--- Assets/Scripts/MenusCrud/MenuCompany/LinksCompany.cs
+++ Assets/Scripts/MenusCrud/MenuCompany/LinksCompany.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class LinksCompany : MonoBehaviour
 {
-   public void OpenYoutubeTutorial()
-    {
+    private const string YOUTUBE_TUTORIALS =
+                                            "https://youtube.com/playlist?list=PLsvltDspdJcfiiWy2baA2MCNzBm32USjv&si=q7dTsZltYs-d3eOI";
+    private const string GIT_HUB = 
+                                "https://github.com/reivaxcorp/app-crud-unity-3d";
 
+    public void OpenYoutubeTutorial()
+    {
+        Application.OpenURL(YOUTUBE_TUTORIALS);
     }
 
     public void OpenGitHubCode()
     {
-
+        Application.OpenURL(GIT_HUB);
     }
 }
